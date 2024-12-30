a52896f826614123e16eec07b9174e30e9bc9ebd
Mohamed <mohamedbentouhami512@gmail.com>
2024-12-30 18:16:02 +0100

    add style to error page

--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -1,7 +1,10 @@
+import notFoundImage from "../../assets/404.svg"
 function Error() {
   return (
-    <div>
-      <h1>Oups 🙈 this page doesn't exist</h1>
+    <div className="error-page">
+      <h2>Oups...</h2>
+      <img src={notFoundImage} alt="not-found" ></img>
+      <h2>Oups 🙈 this page doesn't exist</h2>
     </div>
   );
 }
